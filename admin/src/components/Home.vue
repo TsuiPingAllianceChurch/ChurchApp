906dca5ac64dc6673c8aaf024e4508b5c11831ab
Raymond Cheng <ckwingngai@gmail.com>
2018-12-08 17:09:42 +0800

    Update homepage to be the user page

--- admin/src/components/Home.vue
+++ admin/src/components/Home.vue
@@ -1,15 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <div class="link"><a href="/#/manual_attend">Manual Attend</a></div>
-    <div class="link"><a href="/#/auto_attend">Auto Attend</a></div>
-    <div class="link"><a href="/#/users">Users List</a></div>
-    <!-- <div v-for="(worship, key) in getWorship" :key="key">
-      <input type="radio" :id="worship.worship_id" :value="worship.worship_id" v-model="picked">
-      <label :for="worship.worship_id">{{ worship.date }} - {{ worship.type }}</label>
-    </div>
-    <span>Picked: {{ picked }}</span>
-    <user-table></user-table> -->
+    <users></users>
   </div>
 </template>
 
@@ -24,7 +16,7 @@ export default {
     }
   },
   components: {
-    'user-table': () => import(/* webpackChunkName: "User" */ './User/Table')
+    'users': () => import(/* webpackChunkName: "User" */ './Users')
   },
   computed: {
     ...mapGetters({
@@ -36,42 +28,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-.user-table {
-  margin: 0 auto;
-  width: 100%;
-}
-.user-row {
-  display: flex;
-  color: #808080;
-  background-color: #fff;
-}
-.user-column {
-  flex: 1;
-  height: 60px;
-  line-height: 60px;
-}
-.user-row_head .user-column {
-  color: #fff;
-  background-color: #36304a;
-}
-.user-row:nth-child(even) {
-  background-color: #f5f5f5;
-}
-.link {
-  font-size: 28px;
-}
 </style>
