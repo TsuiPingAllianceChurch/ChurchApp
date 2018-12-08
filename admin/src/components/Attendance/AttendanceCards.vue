1b591b8ad7109d757d25f95830759e088412536c
Albert Chan <achan11@gmail.com>
2018-12-08 19:44:51 +0800

    add cards

---
+++ admin/src/components/Attendance/AttendanceCards.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card-columns" >
+    <div class="card" v-for="(item, key) in attendanceList" :key="key">
+      <img class="card-img-top" :src=item.avator alt="Avator">
+      <div class="card-body">
+        <h5 class="card-title">{{ item.userName }}</h5>
+        <p class="card-text">{{ item.groupName }}</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">#{{ item.attendanceId }} : {{ item.date | moment("from") }}</small>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import _get from 'lodash/get'
+import _map from 'lodash/map'
+
+export default {
+  name: 'attendanceCards',
+  computed: {
+    attendanceList () {
+      let result = []
+      _map(this.getAttendances, attendance => {
+        result.push({
+          attendanceId: attendance.attendance_id,
+          userName: _get(this.getUser(attendance.user_id), 'name_zh-hk', ''),
+          groupName: _get(this.getGroup(attendance.user_id), 'name_zh-hk', ''),
+          avator: _get(this.getUser(attendance.user_id), 'avator', ''),
+          date: attendance.created_date
+        })
+      })
+      return result
+    },
+    ...mapGetters({
+      getGroup: 'getGroup',
+      getUser: 'getUser',
+      getAttendances: 'getAttendances'
+    })
+  }
+}
+</script>
